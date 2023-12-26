771b4d90f6a911a13cecff8d33f5c52bbe50df94
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-12-26 22:54:05 +0530

    socket

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const io = require('socket.io')(8000)
 
 const historyController = async (req, res) => {
     try {
         const userid = req.body.data._id
         const transactions = await mongoose.model('transactions').find({ userId: userid })
+
         res.status(200).json(transactions)
     }
     catch (err) {
